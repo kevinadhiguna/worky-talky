b037b084b22d1b6db661017e7f9d7dadf23f012d
kevinadhiguna <kevin26798@gmail.com>
2020-10-25 13:25:39 +0700

    build: release v0.1

---
+++ controllers/employeeController.js
@@ -0,0 +1,120 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+var router = express.Router();
+
+const Employee = mongoose.model("Employee");
+
+router.get("/", (req, res) => {
+	res.render("employee/addOrEdit", {
+		viewTitle: "Add an Employee",
+	});
+});
+
+router.post("/", (req, res) => {
+	if (req.body._id == "") {
+		insertRecord(req, res);
+	} else {
+		updateRecord(req, res);
+	}
+});
+
+function insertRecord(req, res) {
+	var employee = new Employee();
+
+	employee.fullName = req.body.fullName;
+	employee.email = req.body.email;
+	employee.mobile = req.body.mobile;
+	employee.city = req.body.city;
+
+	console.log(req.body);
+
+	employee.save((err, doc) => {
+		if (!err) {
+			res.redirect("employee/list");
+		} else {
+			if (err.name == "ValidationError") {
+				handleValidationError(err, req.body);
+				res.render("employee/addOrEdit", {
+					viewTitle: "Add an Employee",
+					employee: req.body,
+				});
+			} else {
+				console.log("An Error occured during insertion.. " + err);
+			}
+		}
+	});
+}
+
+function updateRecord(req, res) {
+	Employee.findOneAndUpdate(
+		{ _id: req.body._id },
+		req.body,
+		{ new: true },
+		(err, doc) => {
+			if (!err) {
+				res.redirect("employee/list");
+			} else {
+				if (err.name == "ValidationError") {
+					handleValidationError(err, req.body);
+					res.render("employee/addOrEdit", {
+						viewTitle: "Update Employee",
+						employee: req.body,
+					});
+				} else {
+					console.log("An Error occured during update.. " + err);
+				}
+			}
+		}
+	);
+}
+
+router.get("/list", (req, res) => {
+    Employee.find((err, docs) => {
+        if (!err) {
+            res.render('employee/list', {
+                list: docs,
+            });
+        } else {
+            console.log('An Error occured during retrieving employee list ...' + err);
+        }
+    })
+});
+
+function handleValidationError(err, body) {
+    for (field in err.errors) {
+        switch (err.errors[field].path) {
+            case 'fullName':
+                body['fullNameError'] = err.errors[field].message;
+                break;
+            case 'email':
+                body['emailError'] = err.errors[field].message;
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+router.get("/:id", (req, res) => {
+    Employee.findById(req.params.id, (err, doc) => {
+        if (!err) {
+            res.render("employee/addOrEdit", {
+                viewTitle: "Update an Employee",
+                employee: doc,
+            });
+        }
+    });
+});
+
+router.get("/delete/:id", (req, res) => {
+    Employee.findByIdAndRemove(req.params.id, (err, doc) => {
+        if (!err) {
+            res.redirect('/employee/list');
+        } else {
+            console.log('An Error occured during deleting...' + err);
+        }
+    });
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,38 @@
+require("./models/db");
+
+const express = require("express");
+const path = require("path");
+
+const exphbs = require("express-handlebars");
+const Handlebars = require("handlebars");
+
+const bodyParser = require("body-parser");
+
+const {
+	allowInsecurePrototypeAccess,
+} = require("@handlebars/allow-prototype-access");
+
+const employeeController = require("./controllers/employeeController");
+
+var app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.set("view engine", path.join(__dirname, "/views"));
+app.engine(
+	".hbs",
+	exphbs({
+		extname: ".hbs",
+		defaultLayout: "mainLayout",
+		layoutsDir: __dirname + "/views/layouts",
+		handlebars: allowInsecurePrototypeAccess(Handlebars),
+	})
+);
+app.set("view engine", "hbs");
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+	console.log("Server is running at port " + PORT);
+});
+
+app.use("/employee", employeeController);
